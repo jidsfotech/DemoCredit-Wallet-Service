2d8fcf7763dcec52bea5a5503c467f1ae1c1a1bc
Majeed <jidsfotech@gmail.com>
2026-05-07 10:14:08 +0100

    setup testing for the project

--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,11 @@
 import * as dotenv from 'dotenv';
 import type { Knex } from 'knex';
+import path from 'path';
 
-dotenv.config();
+// Determine which env file to load
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
-console.log({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'mysql2',
@@ -28,7 +24,19 @@ const config: { [key: string]: Knex.Config } = {
       directory: './src/common/database/migrations',
     },
   },
-
+  test: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './src/common/database/migrations',
+    },
+  },
   production: {
     client: 'mysql2',
     connection: process.env.DATABASE_URL,
